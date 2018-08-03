942c4b5ca98e32b50d2d791ddbfe3f539a7c792d
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-03 10:00:08 -0700

    added function to generate words; function checks to see if word has already been chosen

--- index.js
+++ index.js
@@ -1,64 +1,93 @@
 var word = require('./Word.js');
 var inquirer = require('inquirer');
-var numGuesses = 0;
-const MAX_GUESSES = 10;
+const MAX_GUESSES = 12;
 var chosenWords = [];
+var guessedLetters = [];
 var wordArray = ['character', 'logic', 'iteration', 'version', 'data', 'loop'];
 
-
+//function that uses a random number to select a word 
+//from the word array
 function generateWord() {
-    var found = false;
-    var randomNum = 0;
+    var randomNum;
     do {
+        var found = false;
+        console.log('entered do while loop');
         if (chosenWords.length === wordArray.length) {
             chosenWords = [];
         }
         randomNum = Math.floor(Math.random() * wordArray.length);
+        console.log(randomNum);
         var i = 0;
         while (i < chosenWords.length && !found) {
-            if (chosenWords[i] = randomNum) {
+            console.log('entered nested while loop');
+            if (chosenWords[i] === randomNum) {
+                console.log('entered if statement');
                 found = true;
             } else {
                 i++;
             }
         }
     } while (found);
-   
+    console.log('exit do while loop');
+    chosenWords.push(randomNum);
     return new word.Word(wordArray[randomNum]);
 }
 
-function iteration(myWord) {
+//recursive function that handles a round of the game
+//accepts a Word object and the current number of guesses
+//displays the current word string,
+//prompts user to enter a character
+//displays the updated word string
+//checks to see if player won/lost
+function iteration(myWord, numGuesses) {
     console.log(myWord.returnString());
+    console.log('# Guesses: ' + numGuesses);
     var isFound = true;
     inquirer.prompt({
         name: 'guessLetter',
         message: 'Enter your guess: '
     }).then(function(reply) {
-        myWord.searchWord(reply.guessLetter);
-        console.log(myWord.returnString());
         var i = 0;
-        while (i < myWord.letterArray.length && isFound) {
-            if (!myWord.letterArray[i].beenGuessed) {
-                isFound = false;
+        var previouslyGuessed = false;
+        while (i < guessedLetters.length && !previouslyGuessed) {
+            if (guessedLetters[i] === reply.guessLetter) {
+                previouslyGuessed = true;
             } else {
                 i++;
             }
         }
-        if (numGuesses < MAX_GUESSES && !isFound) {
+        if (previouslyGuessed) {
+            console.log(reply.guessLetter + ' has already been guessed.\nPlease guess another letter: ');
+            iteration(myWord, numGuesses);
+        } else {
+            guessedLetters.push(reply.guessLetter);
             numGuesses++;
-            iteration(myWord);
-        } else if (isFound && numGuesses < MAX_GUESSES) {
-            console.log('You win');
-            numGuesses = 0;
-            playAgain();
-        } else if (numGuesses === MAX_GUESSES) {
-            console.log('You lose');
-            numGuesses = 0;
-            playAgain();
+            myWord.searchWord(reply.guessLetter);
+            console.log(myWord.returnString());
+            i = 0;
+            while (i < myWord.letterArray.length && isFound) {
+                if (!myWord.letterArray[i].beenGuessed) {
+                    isFound = false;
+                } else {
+                    i++;
+                }
+            }
+            if (numGuesses < MAX_GUESSES && !isFound) {
+                iteration(myWord, numGuesses);
+            } else if (isFound && numGuesses < MAX_GUESSES) {
+                console.log('You win');
+                guessedLetters = [];
+                playAgain();
+            } else if (numGuesses === MAX_GUESSES) {
+                console.log('You lose');
+                guessedLetters = [];
+                playAgain();
+            }
         }
     });
 }
 
+//function that asks the user if he/she wants to play again
 function playAgain() {
     inquirer.prompt({
         name: 'again',
@@ -66,10 +95,10 @@ function playAgain() {
         type: 'confirm'
     }).then(function(response) {
         if (response.again) {
-            iteration(generateWord());
+            iteration(generateWord(), 0);
         }
     });
 }
 
  
-iteration(generateWord());
+iteration(generateWord(), 0);
